b02dc5a3709ebc3b2629bbbae48d3b8913bd7d08
Vincent Boutour <vincent.boutour@gmail.com>
2016-06-26 12:34:53 +0000

    Updating

--- generate_httpd_conf.sh
+++ generate_httpd_conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p auth
-docker run --entrypoint htpasswd httpd:2.4 -Bbn visitor vistor > auth/httpd.htpasswd
-docker run --entrypoint htpasswd httpd:2.4 -Bbn admin password >> auth/httpd.htpasswd
+docker run --rm --entrypoint htpasswd httpd -Bbn visitor vistor > auth/httpd.htpasswd
+docker run --rm --entrypoint htpasswd httpd -Bbn admin password >> auth/httpd.htpasswd
 echo "pusher: admin" > auth/httpd.groups
